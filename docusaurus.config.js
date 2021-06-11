c5e9c7dfb3201f58ed01ac73e5103f4c0bdfbf62
SkyBlockDev <72335827+SkyBlockDev@users.noreply.github.com>
2021-06-11 08:26:30 +0200

    Intial commit

---
+++ docusaurus.config.js
@@ -0,0 +1,87 @@
+/** @type {import('@docusaurus/types').DocusaurusConfig} */
+module.exports = {
+  title: 'Natico',
+  tagline: 'Natico the all in one discordeno framework',
+  url: 'https://your-docusaurus-test-site.com',
+  baseUrl: '/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.ico',
+  organizationName: 'naticoo', // Usually your GitHub org/user name.
+  projectName: 'natico', // Usually your repo name.
+  themeConfig: {
+    navbar: {
+      title: 'Natico',
+      logo: {
+        alt: 'Natico Logo',
+        src: 'img/logo.svg',
+      },
+      items: [
+        {
+          type: 'doc',
+          docId: 'intro',
+          position: 'left',
+          label: 'Tutorial',
+        },
+
+        {
+          href: 'https://github.com/naticoo/framework',
+          label: 'GitHub',
+          position: 'right',
+        },
+      ],
+    },
+    footer: {
+      style: 'dark',
+      links: [
+        {
+          title: 'Docs',
+          items: [
+            {
+              label: 'Tutorial',
+              to: '/docs/intro',
+            },
+          ],
+        },
+        {
+          title: 'Community',
+          items: [
+
+            {
+              label: 'Discord',
+              href: 'https://discord.gg/KkMKCchJb8',
+            },
+
+          ],
+        },
+        {
+          title: 'More',
+          items: [
+
+            {
+              label: 'GitHub',
+              href: 'https://github.com/naticoo/framework',
+            },
+          ],
+        },
+      ],
+      copyright: `Copyright © ${new Date().getFullYear()} Natico, Inc. Built with Docusaurus.`,
+    },
+  },
+  presets: [
+    [
+      '@docusaurus/preset-classic',
+      {
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+          // Please change this to your repo.
+          editUrl:
+            'https://github.com/naticoo/framework/edit/master/website/',
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      },
+    ],
+  ],
+};
